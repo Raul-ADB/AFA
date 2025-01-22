27bb1a9c8121fb53b33c1b50d34b8a55ddf3aed1
adrian <adriansilverslatter@gmail.com>
2025-01-22 13:59:06 -0600

    fast

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let fondos = [];
-fondos[0] = '../img/in1.jpg';
-fondos[1] = '../img/in2.jpg';
-fondos[2] = '../img/in3.jpg';
-fondos[3] = '../img/in4.jpg';
-fondos[4] = '../img/in5.jpg';
-fondos[5] = '../img/in6.jpg';
+fondos[0] = './img/in1.jpg';
+fondos[1] = './img/in2.jpg';
+fondos[2] = './img/in3.jpg';
+fondos[3] = './img/in4.jpg';
+fondos[4] = './img/in5.jpg';
+fondos[5] = './img/in6.jpg';
 
 window.addEventListener("load", () => {
   let fondo = Math.floor(Math.random() * fondos.length);

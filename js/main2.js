bfe7d9dc915943145f97a3bc1b62895b18d0c395
adrian <adriansilverslatter@gmail.com>
2025-01-29 13:40:31 -0600

    Navbar

--- js/main2.js
+++ js/main2.js
@@ -1,10 +1,16 @@
-const navbar = document.querySelector('#nav');
-let prevState = window.pageYOffset;
-window.addEventListener('scroll', () => {
-  let currentState = window.pageYOffset;
-  if (prevState > currentState) {
-    navbar.style.top = '0';
-  } else {
-    navbar.style.top = '-40rem';
-  }
+let lastScrollTop = 0;
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        // Scroll hacia abajo
+        navbar.style.top = "-100px";  // Esconde la navbar
+    } else {
+        // Scroll hacia arriba
+        navbar.style.top = "0";  // Muestra la navbar
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Evitar que se pase al valor negativo
 });
